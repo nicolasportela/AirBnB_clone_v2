c847ff326f74a0266000febdc57c5ac03d4a82c1
nicolasportela <2103@holbertonschool.com>
2021-04-15 03:11:28 +0000

    task 0 fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 # script that sets up your web servers for the deployment of web_static
 sudo apt-get -y update
-sudo apt-get -y upgrade
 sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
-sudo service nginx start
 mkdir -p /data/
 mkdir -p /data/web_static/
 mkdir -p /data/web_static/releases/
