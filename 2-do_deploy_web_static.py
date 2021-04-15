9c3752a677682f72b3ed91fcc9f3c8a2d4ddd4f5
nicolasportela <2103@holbertonschool.com>
2021-04-15 19:43:46 +0000

    task 2

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Fabric script that distributes an archive
+to your web servers, using the function do_deploy"""
+
+from fabric.api import local, put, env, run
+from os import path
+env.hosts = ['34.74.134.187', '34.74.176.2']
+env.user = "ubuntu"
+
+
+def do_deploy(archive_path):
+    """function to distribute an archive to web server"""
+    if not (path.exists(archive_path)):
+        return False
+    try:
+        put(archive_path, "/tmp/")
+        name = archive_path.split('/')[1].split('.')[0]
+        run("mkdir -p /data/web_static/releases/{}".format(name))
+        run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}"
+            .format(name, name))
+        run("rm /tmp/{}.tgz".format(name))
+        run("mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/".format(name, name))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(name))
+        run("ln -sf /data/web_static/releases/{}/ /data/web_static/current"
+            .format(name))
+        return True
+    except:
+        return False
