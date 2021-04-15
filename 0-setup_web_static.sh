2a64c876112b8fc5a7b2e65047c87146a0bae5f7
nicolasportela <2103@holbertonschool.com>
2021-04-15 03:23:10 +0000

    task 0 fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -13,7 +13,7 @@ sudo chown -R "$USER:$USER" /data/
 new_string="\\\n\tlocation /hbnb_static {\n\t\t alias /data/web_static/current/;\n\t}\n"
 
 check_exist=$(grep "hbnb_static" /etc/nginx/sites-available/default)
-if [[ -z $check_exist ]];
+if [[ -z $check_exist ]]
 then
     sed -i "/server_name _/a $new_string" /etc/nginx/sites-available/default
 fi
