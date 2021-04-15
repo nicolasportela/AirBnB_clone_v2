b9ff4145c7db0b37b8e8914f2e5ddc8cb10b64b7
nicolasportela <2103@holbertonschool.com>
2021-04-15 19:56:54 +0000

    task 2 fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -19,9 +19,9 @@ def do_deploy(archive_path):
         run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}"
             .format(name, name))
         run("rm /tmp/{}.tgz".format(name))
-        run("mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/".format(name, name))
-        run("rm -rf /data/web_static/releases/{}/web_static".format(name))
+        # run("mv /data/web_static/releases/{}/web_static/* \
+        #    /data/web_static/releases/{}/".format(name, name))
+        # run("rm -rf /data/web_static/releases/{}/web_static".format(name))
         run("ln -sf /data/web_static/releases/{}/ /data/web_static/current"
             .format(name))
         return True
