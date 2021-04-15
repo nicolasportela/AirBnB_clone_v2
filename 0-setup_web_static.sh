df1aaf300357c1a6d98b4f6db0ad41ecd05398cd
nicolasportela <2103@holbertonschool.com>
2021-04-15 14:26:37 +0000

    task 0 fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -11,7 +11,13 @@ mkdir -p /data/web_static/
 mkdir -p /data/web_static/releases/
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
-echo "Testing" > /data/web_static/releases/test/index.html
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R "ubuntu:ubuntu" /data/
 new_string="\\\n\tlocation /hbnb_static {\n\t\t alias /data/web_static/current/;\n\t}\n"
