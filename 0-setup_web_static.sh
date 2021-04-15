1de08c7874df366e21943c54961a4f3064d1a28c
nicolasportela <2103@holbertonschool.com>
2021-04-15 03:31:03 +0000

    task 0 fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,7 +9,7 @@ mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
 echo "This is a test" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R "$USER:$USER" /data/
+sudo chown -R "ubuntu:ubuntu" /data/
 new_string="\\\n\tlocation /hbnb_static {\n\t\t alias /data/web_static/current/;\n\t}\n"
 
 check_exist=$(grep "hbnb_static" /etc/nginx/sites-available/default)
