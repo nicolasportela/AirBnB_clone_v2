e59c4e8b16b8a2bb509b2dd24dc5e0143aaee8c9
nicolasportela <2103@holbertonschool.com>
2021-04-17 13:55:01 +0000

    task 0 fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,9 +6,6 @@ then
     sudo apt-get -y upgrade
     sudo apt-get -y install nginx
 fi
-mkdir -p /data/
-mkdir -p /data/web_static/
-mkdir -p /data/web_static/releases/
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
 echo "<html>
@@ -18,12 +15,17 @@ echo "<html>
     Holberton School
   </body>
 </html>" > /data/web_static/releases/test/index.html
+# nice conditional
+# if [[ -e "/data/web_static/current" ]]
+# then
+#	  rm /data/web_static/current
+# fi
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R "ubuntu:ubuntu" /data/
-new_string="\\\n\tlocation /hbnb_static {\n\t\t alias /data/web_static/current/;\n\tautoindex on;\n\t}\n"
-check_exist=$(grep "hbnb_static" /etc/nginx/sites-available/default)
-if [[ -z $check_exist ]]
-then
-  sed -i "/server_name _/a $new_string" /etc/nginx/sites-available/default
-fi
+sudo chown -R ubuntu:ubuntu /data/
+sed -i "48i\ \n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
+# alternative:
+# if [[ -z $check_exist ]]
+# then
+# sed -i "/server_name _/a $new_string" /etc/nginx/sites-available/default
+# fi
 sudo service nginx restart
