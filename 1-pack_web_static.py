b0cc0f61824948ae510facf25af7bcc834f73fbb
nicolasportela <2103@holbertonschool.com>
2021-04-18 05:25:45 +0000

    task 1 edited

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,10 +10,10 @@ def do_pack():
     """function to compress file"""
     local("mkdir -p versions")
     # create the name of file in str format from datetime.now
-    name = "web_static_" + datetime.strftime(datetime.now(),
-                                             "%Y%m%d%H%M%S") + ".tgz"
+    now = datetime.now()
+    name = "versions/web_static_" + now.strftime("%Y%m%d%H%M%S") + ".tgz"
     try:
-        local("tar -czvf ./versions/{} ./web_static" .format(name))
-        return(name)
+        local("tar -czvf " + name + " web_static")
+        return name
     except:
-        return(None)
+        return None
