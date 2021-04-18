a8f3f017678d0341ccc493d1ef60c70b987f82b5
nicolasportela <2103@holbertonschool.com>
2021-04-18 04:42:20 +0000

    just to try another code task 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -28,17 +28,19 @@ def do_deploy(archive_path):
         return False
     try:
         put(archive_path, "/tmp/")
-        name = archive_path.split('/')[1].split('.')[0]
-        run("mkdir -p /data/web_static/releases/{}".format(name))
-        run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}"
-            .format(name, name))
-        run("rm /tmp/{}.tgz".format(name))
-        run("mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/".format(name, name))
-        run("rm -rf /data/web_static/releases/{}/web_static".format(name))
+        """ file_name name of file with .tgz """
+        file_name = archive_path.split("/")[1]
+        """ file_name2 name of file without .tgz """
+        file_name2 = file_name.split(".")[0]
+        """ final_name name of path of directory """
+        final_name = "/data/web_static/releases/" + file_name2 + "/"
+        run("mkdir -p " + final_name)
+        run("tar -xzf /tmp/" + file_name + " -C " + final_name)
+        run("rm /tmp/" + file_name)
+        run("mv " + final_name + "web_static/* " + final_name)
+        run("rm -rf " + final_name + "web_static")
         run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-            .format(name))
+        run("ln -s " + final_name + " /data/web_static/current")
         return True
     except:
         return False
