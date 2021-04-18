b7b25bf422c964044da71290341fde5fde1b3d08
nicolasportela <2103@holbertonschool.com>
2021-04-18 04:12:04 +0000

    task 3 fix

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -24,23 +24,24 @@ def do_pack():
 
 def do_deploy(archive_path):
     """function to distribute an archive to web server"""
-    if not (path.exists(archive_path)):
-        return False
-    try:
-        put(archive_path, "/tmp/")
-        name = archive_path.split('/')[1].split('.')[0]
-        run("mkdir -p /data/web_static/releases/{}".format(name))
-        run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}"
-            .format(name, name))
-        run("rm /tmp/{}.tgz".format(name))
-        run("mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/".format(name, name))
-        run("rm -rf /data/web_static/releases/{}/web_static".format(name))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-            .format(name))
-        return True
-    except:
+    if path.exists(archive_path):
+        try:
+            put(archive_path, "/tmp/")
+            name = archive_path.split('/')[1].split('.')[0]
+            run("mkdir -p /data/web_static/releases/{}".format(name))
+            run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}"
+                .format(name, name))
+            run("rm /tmp/{}.tgz".format(name))
+            run("mv /data/web_static/releases/{}/web_static/* \
+                /data/web_static/releases/{}/".format(name, name))
+            run("rm -rf /data/web_static/releases/{}/web_static".format(name))
+            run("rm -rf /data/web_static/current")
+            run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+                .format(name))
+            return True
+        except:
+            return False
+    else:
         return False
 
 
