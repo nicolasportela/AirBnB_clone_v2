b8e1f14ac6fff93a193b63e4dbc2478b1d539d3b
nicolasportela <2103@holbertonschool.com>
2021-05-02 15:08:56 +0000

    task 6 permissions and task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+
+from flask import Flask
+from flask import render_template
+from models import storage
+app = Flask(__name__)
+
+@app.route('/states_list', strict_slashes=False)
+def html4():
+    """function 8 to display an html page"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage"""
+    storage.close()
+
+if __name__ == '__main__':
+    app.run()
